f0e78d2dacfcc4e60d7c423b1e020f519a6d630e
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-17 00:09:17 -0300

    desenvolvido a model tbProduto

---
+++ CrudApplication/Models/tbProduto.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudApplication.Models
+{
+    public class tbProduto
+    {
+        public int ?IdProduto { get; set; }
+        public string ?NomeProduto { get; set; }
+        public string ?Descricao { get; set; }
+        public decimal ?Preco { get; set; }
+        public int ?Quantidade { get; set; }
+
+        public List<tbProduto> ?ListaProduto { get; set; }
+    }
+}
