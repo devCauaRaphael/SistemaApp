c1d272b29140e30aa471a78b44a339fb632a5615
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-17 00:11:34 -0300

    desenvolvido o construtor do repositorio funcionario

--- CrudApplication/Controllers/FuncionarioController.cs
+++ CrudApplication/Controllers/FuncionarioController.cs
@@ -4,6 +4,13 @@ namespace CrudApplication.Controllers
 {
     public class FuncionarioController : Controller
     {
+        FuncionarioRepositorio _FuncionarioRepositorio;
+
+        public FuncionarioController(FuncionarioRepositorio funcionarioRepositorio)
+        {
+            _FuncionarioRepositorio = funcionarioRepositorio;
+        }
+
         public IActionResult Index()
         {
             return View();
