ff6219e2f8c200fcbbce499c04b1c22a3085fd56
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-17 01:23:53 -0300

    desenvolvido o método para atualizar funcionario

--- CrudApplication/Repositorios/FuncionarioRepositorio.cs
+++ CrudApplication/Repositorios/FuncionarioRepositorio.cs
@@ -13,7 +13,7 @@ namespace CrudApplication.Repositorios
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
                 conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("insert into produto(Nome, Email, Senha) values (@nome, @email, @senha)", conexao);
+                MySqlCommand cmd = new MySqlCommand("insert into funcionario(Nome, Email, Senha) values (@nome, @email, @senha)", conexao);
                 cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = funcionario.Nome;
                 cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = funcionario.Email;
                 cmd.Parameters.Add("@senha", MySqlDbType.String).Value = funcionario.Senha;
@@ -21,5 +21,28 @@ namespace CrudApplication.Repositorios
                 conexao.Close();
             }
         }
+        public bool AtualizarFuncionario(tbFuncionario funcionario)
+        {
+            try
+            {
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
+                    MySqlCommand cmd = new MySqlCommand("Update funcionario set Nome=@nome, Email=@email, Senha=@senha" + " where IdFuncionario=@codigo", conexao);
+                    cmd.Parameters.Add("@codigo", MySqlDbType.Int32).Value = funcionario.IdFuncionario;
+                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = funcionario.Nome;
+                    cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = funcionario.Email;
+                    cmd.Parameters.Add("@senha", MySqlDbType.String).Value = funcionario.Senha;
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                    return linhasAfetadas > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao atualizar funcionario: {ex.Message}");
+                return false;
+            }
+        }
     }
+
 }
