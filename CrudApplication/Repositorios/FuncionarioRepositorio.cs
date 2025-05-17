6e76433341e73919658d60b40ea99f639040531d
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-17 09:49:16 -0300

    desenvolvido a estrutura da view EditarFuncionario

--- CrudApplication/Repositorios/FuncionarioRepositorio.cs
+++ CrudApplication/Repositorios/FuncionarioRepositorio.cs
@@ -14,7 +14,7 @@ namespace CrudApplication.Repositorios
             using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
                 conexao.Open();
-                MySqlCommand cmd = new MySqlCommand("insert into funcionario(Nome, Email, Senha) values (@nome, @email, @senha)", conexao);
+                MySqlCommand cmd = new MySqlCommand("insert into tbFuncionario(Nome, Email, Senha) values (@nome, @email, @senha)", conexao);
                 cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = funcionario.Nome;
                 cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = funcionario.Email;
                 cmd.Parameters.Add("@senha", MySqlDbType.String).Value = funcionario.Senha;
@@ -29,7 +29,7 @@ namespace CrudApplication.Repositorios
                 using (var conexao = new MySqlConnection(_conexaoMySQL))
                 {
                     conexao.Open();
-                    MySqlCommand cmd = new MySqlCommand("Update funcionario set Nome=@nome, Email=@email, Senha=@senha" + " where IdFuncionario=@codigo", conexao);
+                    MySqlCommand cmd = new MySqlCommand("Update tbFuncionario set Nome=@nome, Email=@email, Senha=@senha" + " where IdFuncionario=@codigo", conexao);
                     cmd.Parameters.Add("@codigo", MySqlDbType.Int32).Value = funcionario.IdFuncionario;
                     cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = funcionario.Nome;
                     cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = funcionario.Email;
