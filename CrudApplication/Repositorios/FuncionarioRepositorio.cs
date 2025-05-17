1a6db8c5f14d055a61042d16663b45e937e90372
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-17 01:19:28 -0300

    desenvolvido o método para cadastrar funcionario

--- CrudApplication/Repositorios/FuncionarioRepositorio.cs
+++ CrudApplication/Repositorios/FuncionarioRepositorio.cs
@@ -1,6 +1,25 @@
-﻿namespace CrudApplication.Repositorios
+﻿using CrudApplication.Models;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace CrudApplication.Repositorios
 {
-    public class FuncionarioRepositorio
+    public class FuncionarioRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public void CadastrarFuncionario(tbFuncionario funcionario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("insert into produto(Nome, Email, Senha) values (@nome, @email, @senha)", conexao);
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = funcionario.Nome;
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = funcionario.Email;
+                cmd.Parameters.Add("@senha", MySqlDbType.String).Value = funcionario.Senha;
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
     }
 }
