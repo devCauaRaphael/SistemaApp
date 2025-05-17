57ccff5cb61cf9e4157ab356235fc128316d9ad2
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-05-17 00:04:45 -0300

    desenvolvido o banco de dados

---
+++ script.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE dbCrudApplication;
+USE dbCrudApplication;
+
+CREATE TABLE tbFuncionario(
+	IdFuncionario int primary key auto_increment,
+    Email varchar(50) not null,
+    Nome varchar(50) not null,
+    Senha char(16) not null
+);
+
+CREATE TABLE tbProduto(
+	IdProduto int primary key auto_increment,
+    NomeProduto varchar(50) not null,
+    Quantidade int not null,
+    Preco decimal(8,2) not null
+);
+
+INSERT INTO tbFuncionario(Email, Nome, Senha) VALUES ('adm@gmail.com', 'admin', 123);
+SELECT * FROM tbFuncionario;
+SELECT * FROM tbProduto;
